55096f23c8ea074a7eb3a75e6cd4872c2f3b312e
Tamika Ross <tamika.ross.xu@gmail.com>
2025-03-02 11:08:55 -0600

    still updating

--- game.py
+++ game.py
@@ -20,13 +20,25 @@ def snowman(snowman_word):
     If the player wins and, 
     'Sorry, you lose! The word was {snowman_word}' if the player loses
     """
-    correct_letter_guess = build_letter_status_dict(snowman_word)
+    correct_letter_guess_statuses = build_letter_status_dict(snowman_word)
     wrong_guesses_list = []
 
     for wrong_guess_count in range(SNOWMAN_MAX_WRONG_GUESSES):
         print_word_progress_string(snowman_word, correct_letter_guess_statuses)
         print_snowman_graphic(wrong_guess_count)
         guess = get_letter_from_user(correct_letter_guess_statuses, wrong_guesses_list)
+
+        if guess in snowman_word:
+            correct_letter_guess_statuses[guess] = True 
+        else: 
+            wrong_guesses_list.append[guess] = True 
+            print("Wrong guess!")
+        if is_word_guessed(snowman_word, correct_letter_guess_statuses):
+            print("Congratulations, you win!")
+            return
+        
+    print(f"Sorry, you lose! The word was {snowman_word}")
+
 
 
 def print_snowman_graphic(wrong_guesses_count):
